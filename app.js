2688bf860a57c626ab00b201fdae33e7f3da5d97
Pavlo <pasha.hrendzha@ukr.net>
2021-08-19 14:35:12 +0300

    first-commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const exhbs = require('express-handlebars');
+const products = require('./products.json');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine(
+    'hbs',
+    exhbs({
+        extname: 'hbs',
+    }),
+);
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About',
+    });
+});
+
+app.get('/products', (req, res) => {
+    res.render('products', {
+        products,
+        cssFileName: 'products',
+        pageTitle: 'Products',
+    });
+});
+
+app.get('/product/:productId', (req, res) => {
+    const product = products.find(product => product.id === req.params.productId);
+
+    res.render('product', {
+        product,
+        pageTitle: `${product.name}`,
+    });
+});
+
+app.listen(4444, () => {
+    console.log(`app running on port ${4444}`);
+});
